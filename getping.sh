3037e958f542373c336bf23a8016ddc1687c100b
Jake Parnell (CMP) <qbz17rgu@uea.ac.uk>
2019-03-25 15:12:58 +0000

    modified ping packet send count

--- getping.sh
+++ getping.sh
@@ -3,5 +3,5 @@ for ((c=0; c<2; c++)); do
 	now=$(date +"%Y-%m-%d-%T")
 	filename="${array[${c}]}|ping|$now.log"
 	echo $filename
-	ping -c 10 -n ${array[${c}]} > "$filename"
+	ping -c 120 -n ${array[${c}]} > "$filename"
 done
